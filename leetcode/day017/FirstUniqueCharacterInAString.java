df8662265355983fbd8fc3d6ae823ee6b9a07353
yash golwara <yash.golwara@siemens.com>
2021-01-19 22:58:39 +0530

    Day17 of 100DaysOfCode

---
+++ leetcode/day017/FirstUniqueCharacterInAString.java
@@ -0,0 +1,19 @@
+package day017;
+
+public class FirstUniqueCharacterInAString {
+	public int firstUniqChar(String s) {
+		int[] count = new int[26];
+		int n = s.length();
+		for (int i = 0; i < n; i++) {
+			count[s.charAt(i) - 'a']++;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (count[s.charAt(i) - 'a'] == 1) {
+				return i;
+			}
+
+		}
+		return -1;
+	}
+}
